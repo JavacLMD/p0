201aae2680df94c4bdc1355978bc1d14bed20cc6
JavacLMD <dorscherl@gmail.com>
2021-07-22 18:39:19 -0500

    project initialization

--- src/main/java/PetManagement.java
+++ src/main/java/PetManagement.java
@@ -1,11 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PetManagement {
 
+    public static String connectionName = "jdbc:sqlite:PetCustomers.db";
+
+
+
     public static void main(String[] args) {
+        //establish data base connection
+        try (Connection connection = DriverManager.getConnection(connectionName)) {
 
+            connection.prepareStatement("create table customers (id INT PRIMARY");
 
-        
+        } catch (SQLException e){
 
-
+        }
     }
+
 
 }
